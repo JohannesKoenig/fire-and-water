c17d763347b86e3bde662e6cd3170fff43de5b5f
Johannes König <koenig.nexus@gmail.com>
2024-08-07 10:18:31 +0200

    add level transition script

---
+++ game/scripts/level.gd
@@ -0,0 +1,3 @@
+class_name Level extends Node2D
+
+@export var level_name: String
---
+++ game/scripts/level_transitions.gd
@@ -0,0 +1,27 @@
+class_name LevelTransitions extends Node
+
+var LEVEL_LAYOUT = "res://resources/level_layout.json"
+var data
+
+func load_from_file():
+	if data == null:
+		return
+	
+	var json = JSON.new()
+	var error = json.parse(LEVEL_LAYOUT)
+	if error == OK:
+		data = json.data
+
+func get_next_level(current_level: String) -> Array[String]:
+	load_from_file()
+	var transitions: Array = data["transitions"]
+	var transitions_from_current_level = transitions.filter(
+		func(transition): return transition["from"] == current_level
+	)
+	return transitions_from_current_level.map(
+		func(transition): return transition["to"]
+	)
+	
+func get_first_level() -> String:
+	load_from_file()
+	return data["first_level"]
