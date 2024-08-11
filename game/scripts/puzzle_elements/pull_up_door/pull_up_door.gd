817ff15fb8bef64d5fba851d023d7bc9bad5cfd3
Johannes König <koenig.nexus@gmail.com>
2024-08-11 11:40:21 +0200

    make pull up door work with intermediate opening and closing states

--- game/scripts/puzzle_elements/pull_up_door/pull_up_door.gd
+++ game/scripts/puzzle_elements/pull_up_door/pull_up_door.gd
@@ -2,11 +2,12 @@ class_name PullUpDoor extends Node2D
 
 @onready var animation_player = $AnimationPlayer
 @export var current_animation = "closed"
+
+
 var start_next = true
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
 	if start_next:
-		animation_player.play(current_animation)
 		start_next = false
 	if not animation_player.is_playing():
 		match current_animation:
@@ -20,10 +21,22 @@ func _process(delta):
 	
 
 func close():
+	var remaining = 0
+	if current_animation == "opening":
+		remaining = current_animation_remaining_time()
 	current_animation = "closing"
-	start_next = true
+	animation_player.play(current_animation)
+	animation_player.advance(remaining)
 
 func open():
+	var remaining = 0
+	if current_animation == "closing":
+		remaining = current_animation_remaining_time()
 	current_animation = "opening"
-	start_next = true
+	animation_player.play(current_animation)
+	animation_player.advance(remaining)
 
+func current_animation_remaining_time():
+	var current_animation_progress = animation_player.current_animation_position
+	var current_animation_time = animation_player.current_animation_length
+	return current_animation_time - current_animation_progress
