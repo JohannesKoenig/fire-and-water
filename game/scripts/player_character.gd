a8ffc942f993477718d662d875c3e6597abccb95
Johannes König <koenig.nexus@gmail.com>
2024-08-13 09:57:45 +0200

    add cojote time to jumps

--- game/scripts/player_character.gd
+++ game/scripts/player_character.gd
@@ -7,6 +7,10 @@ const JUMP_VELOCITY = -300.0
 
 @onready var player_input: PlayerInput = $PlayerInput
 @onready var rig = $Rig
+@onready var cojote_timer = $CojoteTimer
+
+var was_on_floor: bool
+var jumping: bool = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -25,12 +29,19 @@ func _process(delta):
 func _physics_process(delta):
 	var input_package = player_input.get_input()
 	var direction = input_package.direction
+	if is_on_floor():
+		jumping = false
 	# Add the gravity.
+	if was_on_floor != is_on_floor():
+		cojote_timer.start()
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if "jump" in input_package.actions and is_on_floor():
+	if "jump" in input_package.actions and can_jump():
+		jumping = true
+		cojote_timer.stop()
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -40,7 +51,8 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	was_on_floor = is_on_floor()
 	move_and_slide()
 	
 	input_package.queue_free()
@@ -50,3 +62,6 @@ func set_player_id(id: int):
 
 func kill():
 	dead.emit()
+
+func can_jump() -> bool:
+	return is_on_floor() or not cojote_timer.is_stopped() and not jumping
