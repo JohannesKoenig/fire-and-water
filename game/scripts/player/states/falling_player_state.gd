0bba77a545b0caf0f3da7468a01a0468e4d057e0
Johannes König <koenig.nexus@gmail.com>
2024-08-16 14:24:11 +0200

    hotfix player movement

--- game/scripts/player/states/falling_player_state.gd
+++ game/scripts/player/states/falling_player_state.gd
@@ -2,17 +2,21 @@ class_name FallingPlayerState extends PlayerState
 
 const SPEED = 120
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var player_started_on_floor: bool = false
 
 @export var cojote_timer: float = 0.15
 
 func transition(input: InputPackage) -> String:
-	if not has_passed(cojote_timer):
+	if player_started_on_floor and not has_passed(cojote_timer):
 		if "Jump" in input.actions:
 			return "Jump"
 	if player.is_on_floor():
 		return "Landing"
 	return name
+
+func on_enter():
+	super()
+	player_started_on_floor = player.is_on_floor()
 	
 func update(input: InputPackage, delta: float):
 	var direction = input.direction
