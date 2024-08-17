2f82481de2aadf7f6b9f4a85ecd018131befddf1
Johannes König <koenig.nexus@gmail.com>
2024-08-17 06:41:06 +0200

    move physics into isolated transform for projectiles and delete when not needed

--- game/scripts/projectile.gd
+++ game/scripts/projectile.gd
@@ -1,6 +1,8 @@
-class_name Projectile extends RigidBody2D
+class_name Projectile extends Node2D
 
 @onready var sprite_2d = $Sprite2D
+@onready var rigid_body_2d = $Physics/RigidBody2D
+@onready var physics = $Physics
 
 @export var time_until_queue_free: float = 1
 @export var time_to_live: float = 1
@@ -11,11 +13,14 @@ signal has_collided
 
 var collision_time: float
 var collided: bool = false
+var can_remove_physics: bool = false
 
 var spawn_time: float
 
 func _ready():
 	spawn_time = Time.get_unix_time_from_system()
+	
+	rigid_body_2d.global_position = global_position
 
 func assemble(target_velocity: Vector2):
 	self.target_velocity = target_velocity
@@ -28,20 +33,28 @@ func _process(delta):
 			collide()
 	
 func _physics_process(delta):
+	if can_remove_physics:
+		remove_physics()
+	if rigid_body_2d != null:
+		position = rigid_body_2d.position
 	if collided:
 		var delta_time = Time.get_unix_time_from_system() - collision_time
 		if delta_time > time_until_queue_free:
 			queue_free()
 	else:
-		set_axis_velocity(self.target_velocity)
+		rigid_body_2d.set_axis_velocity(self.target_velocity)
 
 func _on_body_entered(body):
 	collide()
 
 func collide():
 	if !collided:
-		sleeping = true
+		can_remove_physics = true
 		sprite_2d.visible = false
 		collided = true
 		collision_time = Time.get_unix_time_from_system()
 		has_collided.emit()
+
+func remove_physics():
+	physics.call_deferred("queue_free")
+	can_remove_physics = false
--- game/scripts/projectile_emitter.gd
+++ game/scripts/projectile_emitter.gd
@@ -2,6 +2,6 @@ class_name ProjectileEmitter extends Node2D
 
 
 func emit_projectile(projectile: Projectile):
-	get_tree().root.add_child(projectile)
 	projectile.global_position = global_position
+	get_tree().root.add_child(projectile)
 
