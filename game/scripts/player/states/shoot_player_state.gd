de535c30bb6a5333fadf2529daddef4da6435657
Johannes König <koenig.nexus@gmail.com>
2024-08-18 11:38:07 +0200

    play sounds in game

--- game/scripts/player/states/shoot_player_state.gd
+++ game/scripts/player/states/shoot_player_state.gd
@@ -8,6 +8,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var transition_time: float = 0.5
 @export var shooting_delay: float = 0.2
 
+signal shoot
+
 var has_shot: bool = false
 
 func transition(input: InputPackage) -> String:
@@ -30,6 +32,7 @@ func update(input: InputPackage, delta: float):
 		player.elemental_ball_projectile_emitter.emit_elemental_ball(
 			player.current_element, projectile_direction * PROJECTILE_VELOCITY
 		)
+		shoot.emit()
 		has_shot = true
 	var direction = input.direction
 	if direction:
--- game/scripts/player_visuals.gd
+++ game/scripts/player_visuals.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-@export var rig: Rig
-@onready var animated_sprite_2d = $AnimatedSprite2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var player_audio = $PlayerAudio
+
+@export var rig: Rig
 @export var player_input : PlayerInput
 
 var _player_state_machine: PlayerStateMachine
 
 func accept_model(player_state_machine: PlayerStateMachine):
 	_player_state_machine = player_state_machine
-
+	player_audio.accept_model(player_state_machine)
 
 func _process(delta):
 	var input_package = player_input.get_input()
