540ee7caa6929383c510bce9406f3f5698f4b567
Johannes König <koenig.nexus@gmail.com>
2024-08-23 21:36:49 +0200

    add animation and sound

--- game/scripts/player_character.gd
+++ game/scripts/player_character.gd
@@ -7,7 +7,7 @@ class_name PlayerCharacter extends CharacterBody2D
 @onready var player_state_machine = $PlayerStateMachine
 @onready var elemental_ball_projectile_emitter = $Rig/ElementalBallProjectileEmitter
 
-var has_key: bool = false
+var has_key: bool = true
 
 @export var current_element: String = "Fire":
 	set(value):
--- game/scripts/puzzle_elements/lock_console/lock_console.gd
+++ game/scripts/puzzle_elements/lock_console/lock_console.gd
@@ -1,11 +1,13 @@
 class_name LockConsole extends Node2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var point_light_2d = $PointLight2D
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 var is_locked: bool = true:
 	set(value):
 		is_locked = value
-		if locked:
+		if is_locked:
 			locked.emit()
 		else:
 			unlocked.emit()
@@ -13,12 +15,21 @@ var is_locked: bool = true:
 signal unlocked
 signal locked
 
+func _ready():
+	point_light_2d.energy = 0
+
+
 func unlock():
-	is_locked = true
+	if not is_locked:
+		return
+	is_locked = false
 	animation_player.play("unlock")
+	audio_stream_player_2d.play()
 
 func deactivate():
-	is_locked = false
+	if is_locked:
+		return
+	is_locked = true
 	animation_player.play("lock")
 
 
@@ -26,6 +37,6 @@ func _on_area_2d_area_entered(area):
 	var parent = area.get_parent()
 	if parent is PlayerCharacter:
 		var player: PlayerCharacter = parent
-		if player.has_key:
+		if player.has_key and is_locked:
 			unlock()
 			player.has_key = false
