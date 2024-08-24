500969b5dd4bc49e8db78efd13fa4e1e208f0ae8
Clara.Morrissey <clara-doreen.morrissey@stud.uni-bamberg.de>
2024-08-24 21:17:35 +0200

    add key

--- game/scripts/collectables.gd
+++ game/scripts/collectables.gd
@@ -1,13 +1,15 @@
 extends Area2D
 
-
+@onready var animation_player = $AnimationPlayer
 
 
 func _on_area_entered(area):
 	var parent = area.get_parent()
 	if parent is PlayerCharacter:
 		var player: PlayerCharacter = parent
+		animation_player.play("on_collection")
 		player.set_key_collection()
-	queue_free()
+		
+
 	
 
