1d90b150a84748da68e72bc2b47c91bb67e67b07
Johannes König <koenig.nexus@gmail.com>
2024-08-25 14:06:24 +0200

    connect finish condition to altar

--- game/scripts/diamond_altar_finish_condition.gd
+++ game/scripts/diamond_altar_finish_condition.gd
@@ -1,5 +1,6 @@
 class_name DiamondAltarFinishCondition extends FinishCondition
 
+@export var diamond_altar: DiamondAltar
 
 func is_satisfied() -> bool:
-	return false
+	return diamond_altar.fire_diamond_placed and diamond_altar.water_diamond_placed
