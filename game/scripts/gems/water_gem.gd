33b94cf5282ab8e152f13de018498b49fc14c27e
Johannes König <koenig.nexus@gmail.com>
2024-08-29 08:03:14 +0200

    add gems

---
+++ game/scripts/gems/water_gem.gd
@@ -0,0 +1,13 @@
+class_name WaterGem extends Node2D
+@onready var animation_player = $AnimationPlayer
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
+
+
+func _on_area_2d_area_entered(area):
+	var parent = area.get_parent()
+	if parent is PlayerCharacter:
+		if parent.current_element == "Water":
+			parent.has_gem = true
+			animation_player.play("collect")
+			collision_shape_2d.set_deferred("disabled", true)
+			SaveGameManager.current_level_resource.collected_water_gem = true
