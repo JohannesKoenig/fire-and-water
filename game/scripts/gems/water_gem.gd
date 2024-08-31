c4f78cee623f87c70d225a9746e859e0a846abcf
Johannes König <koenig.nexus@gmail.com>
2024-08-31 12:52:47 +0200

    add fire gem level

--- game/scripts/gems/water_gem.gd
+++ game/scripts/gems/water_gem.gd
@@ -2,6 +2,7 @@ class_name WaterGem extends Node2D
 @onready var animation_player = $AnimationPlayer
 @onready var collision_shape_2d = $Area2D/CollisionShape2D
 
+signal picked_up
 
 func _on_area_2d_area_entered(area):
 	var parent = area.get_parent()
@@ -11,3 +12,4 @@ func _on_area_2d_area_entered(area):
 			animation_player.play("collect")
 			collision_shape_2d.set_deferred("disabled", true)
 			SaveGameManager.current_level_resource.collected_water_gem = true
+			picked_up.emit()
